e1a02da7bde4de0531cb90eaacbeab0cf9c0c7ed
Sarah Haboeck <sarah.haboeck@htl-wels.at>
2020-10-08 13:00:53 +0200

    2 while + scanner

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         int a = -1;
         boolean bool = true;
         int b = -1;
-
+        while (a== -1 && b == -1){
             System.out.print("Bitte geben sie eine Zahl ein:");
-        try {
-            a = scanner.nextInt();
-        } catch (Exception ex) {
-            System.out.println("Ihre eingabe enhält nicht zugelasse Zeichen.");
-        }
-        System.out.print("Bitte geben sie einen operanten (- = false oder + = true) ein:");
-        try {
-            bool = scanner.nextBoolean();
-        } catch (Exception ex) {
-            System.out.println("Ihre eingabe enhält nicht zugelasse Zeichen.");
-        }
-        System.out.print("Bitte geben sie eine zweite Zahl ein:");
-        try{
-            b = scanner.nextInt();
-        } catch (Exception ex) {
-            System.out.println("Ihre eingabe enhält nicht zugelasse Zeichen.");
+            try {
+                Scanner scanner = new Scanner(System.in);
+                a = scanner.nextInt();
+            } catch (Exception ex) {
+                System.out.println("Ihre eingabe enhält nicht zugelasse Zeichen.");
+            }
+            System.out.print("Bitte geben sie einen operanten (- = false oder + = true) ein:");
+            try {
+                Scanner scanner = new Scanner(System.in);
+                bool = scanner.nextBoolean();
+            } catch (Exception ex) {
+                System.out.println("Ihre eingabe enhält nicht zugelasse Zeichen.");
+            }
+            System.out.print("Bitte geben sie eine zweite Zahl ein:");
+            try {
+                Scanner scanner = new Scanner(System.in);
+                b = scanner.nextInt();
+            } catch (Exception ex) {
+                System.out.println("Ihre eingabe enhält nicht zugelasse Zeichen.");
+            }
         }
         int e;
         if (bool)
